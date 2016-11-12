7567fd4625ec437a6aeab58e8c5048e125df7533
KaiN <kain@piwnica.ws>
2016-11-12 16:05:12 +0100

    Added paletteDim function
    Based on OpenFire commit 0fb54eae7cac51431680951552e031a8919e330a

--- ace/utils/palette.c
+++ ace/utils/palette.c
@@ -16,3 +16,23 @@ void paletteLoad(char *szFileName, UWORD *pPalette, UBYTE ubMaxLength) {
 	
 	logBlockEnd("paletteLoad()");
 }
+
+void paletteDim(UWORD *pSource, UWORD *pDest, UBYTE ubColorCount, UBYTE ubLevel) {
+	UBYTE i, c;
+	UBYTE r,g,b;
+	
+	for(c = 0; c != ubColorCount; ++c) {
+		// Extract channels
+		r = (pSource[c] >> 8) & 0xF;
+		g = (pSource[c] >> 4) & 0xF;
+		b = (pSource[c])      & 0xF;
+		
+		// Dim color
+		r = ((r * ubLevel)/15) & 0xF;
+		g = ((g * ubLevel)/15) & 0xF;
+		b = ((b * ubLevel)/15) & 0xF;
+		
+		// Output
+		pDest[c] = (r << 8) | (g << 4) | b;
+	}
+}
--- ace/utils/palette.h
+++ ace/utils/palette.h
@@ -26,4 +26,18 @@ void paletteLoad(
 	IN UBYTE ubMaxLength
 );
 
+/**
+ *  @brief Dims palette to given brightness level.
+ *  @param pSource      Pointer to source palette.
+ *  @param pDest        Pointer to destination palette. May be same as pSource.
+ *  @param ubColorCount Number of colors in palette.
+ *  @param ubLevel      Brightness level - 15 for no dim, 0 for total blackness.
+ */
+void paletteDim(
+	IN UWORD *pSource,
+	OUT UWORD *pDest,
+	IN UBYTE ubColorCount,
+	IN UBYTE ubLevel
+);
+
 #endif
