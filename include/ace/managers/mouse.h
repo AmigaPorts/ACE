4b2aee193b8a74303cae6a55f5e1b348cd0c08c4
KaiN <kain@piwnica.ws>
2017-12-17 17:49:24 +0100

    Dual mice support

--- include/ace/managers/mouse.h
+++ include/ace/managers/mouse.h
@@ -13,17 +13,29 @@
 #define MOUSE_USED 1
 #define MOUSE_ACTIVE 2
 
-typedef struct {
-	UBYTE pButtonStates[3];
+// #define MOUSE_PORT_0 0 // Unused mouse port for alignment
+#define MOUSE_PORT_1 1
+#define MOUSE_PORT_2 2
+
+typedef struct _tMouse {
 	UWORD uwX;
 	UWORD uwY;
+	UBYTE pButtonStates[3];
 #ifdef AMIGA
-	UWORD uwMinX;
-	UWORD uwMinY;
-	UWORD uwMaxX;
-	UWORD uwMaxY;
+	UBYTE ubPrevHwX;
+	UBYTE ubPrevHwY;
+#endif
+} tMouse;
+
+typedef struct _tMouseManager {
+	UBYTE ubPortFlags;
+	tMouse pMouses[3]; // zero is unused, faster than subtracting from port code
+	UWORD uwLoX;
+	UWORD uwLoY;
+	UWORD uwHiX;
+	UWORD uwHiY;
+#ifdef AMIGA
 	UWORD uwPrevPotGo;
-	__chip UWORD pBlankCursor[6];
 #endif // AMIGA
 } tMouseManager;
 
@@ -32,71 +44,114 @@ extern tMouseManager g_sMouseManager;
 
 /* Functions */
 
-void mouseCreate(void);
+void mouseCreate(
+	IN UBYTE ubPortFlags
+);
 
 void mouseDestroy(void);
 
 void mouseProcess(void);
 
+/**
+ * Set on-screen constraints for cursor.
+ * @param uwLoX Minimum cursor X position.
+ * @param uwLoX Minimum cursor Y position.
+ * @param uwHiY Maximum cursor X position.
+ * @param uwHiY Maximum cursor Y position.
+ */
 void mouseSetBounds(
-	IN UWORD uwX,
-	IN UWORD uwY,
-	IN UWORD uwWidth,
-	IN UWORD uwHeight
+	IN UWORD uwLoX,
+	IN UWORD uwLoY,
+	IN UWORD uwHiX,
+	IN UWORD uwHiY
 );
 
-static inline UWORD mouseGetX(void) {
-	return g_sMouseManager.uwX;
+static inline UWORD mouseGetX(
+	IN UBYTE ubMousePort
+) {
+	return g_sMouseManager.pMouses[ubMousePort].uwX;
 }
 
-static inline UWORD mouseGetY(void) {
-	return g_sMouseManager.uwY;
+static inline UWORD mouseGetY(
+	IN UBYTE ubMousePort
+) {
+	return g_sMouseManager.pMouses[ubMousePort].uwY;
 }
 
-static inline void mouseSetButton(UBYTE ubMouseCode, UBYTE ubMouseState) {
-	g_sMouseManager.pButtonStates[ubMouseCode] = ubMouseState;
+static inline void mouseSetButton(
+	IN UBYTE ubMousePort,
+	IN UBYTE ubMouseCode,
+	IN UBYTE ubMouseState
+) {
+	g_sMouseManager.pMouses[ubMousePort].pButtonStates[ubMouseCode] = ubMouseState;
 }
 
-static inline UBYTE mouseCheck(UBYTE ubMouseCode) {
-	return g_sMouseManager.pButtonStates[ubMouseCode] != MOUSE_NACTIVE;
+static inline UBYTE mouseCheck(
+	IN UBYTE ubMousePort,
+	IN UBYTE ubMouseCode
+) {
+	UBYTE ubBtn = g_sMouseManager.pMouses[ubMousePort].pButtonStates[ubMouseCode];
+	return ubBtn != MOUSE_NACTIVE;
 }
 
-static inline UBYTE mouseUse(UBYTE ubMouseCode) {
-	if (g_sMouseManager.pButtonStates[ubMouseCode] == MOUSE_ACTIVE) {
-		g_sMouseManager.pButtonStates[ubMouseCode] = MOUSE_USED;
+static inline UBYTE mouseUse(
+	IN UBYTE ubMousePort,
+	IN UBYTE ubMouseCode
+) {
+	tMouse *pMouse = &g_sMouseManager.pMouses[ubMousePort];
+	if(pMouse->pButtonStates[ubMouseCode] == MOUSE_ACTIVE) {
+		pMouse->pButtonStates[ubMouseCode] = MOUSE_USED;
 		return 1;
 	}
 	return 0;
 }
 
-static inline UBYTE mouseInRect(UWORD uwX, UWORD uwY, UWORD uwWidth, UWORD uwHeight) {
+static inline UBYTE mouseInRect(
+	IN UBYTE ubMousePort,
+	IN UWORD uwX,
+	IN UWORD uwY,
+	IN UWORD uwWidth,
+	IN UWORD uwHeight
+) {
+	UWORD uwMouseX = g_sMouseManager.pMouses[ubMousePort].uwX;
+	UWORD uwMouseY = g_sMouseManager.pMouses[ubMousePort].uwY;
 	return (
-		(uwX <= g_sMouseManager.uwX) && (g_sMouseManager.uwX < uwX + uwWidth) &&
-		(uwY <= g_sMouseManager.uwY) && (g_sMouseManager.uwY < uwY + uwHeight)
+		(uwX <= uwMouseX) && (uwMouseX < uwX + uwWidth) &&
+		(uwY <= uwMouseY) && (uwMouseY < uwY + uwHeight)
 	);
 }
 
 /**
  *  Sets mouse position to given absolute position.
  */
-static inline void mouseSetPosition(UWORD uwNewX, UWORD uwNewY) {
-	g_sMouseManager.uwX = CLAMP(
-		uwNewX, g_sMouseManager.uwMinX, g_sMouseManager.uwMaxX
+static inline void mouseSetPosition(
+	IN UBYTE ubMousePort,
+	IN UWORD uwNewX,
+	IN UWORD uwNewY
+) {
+	g_sMouseManager.pMouses[ubMousePort].uwX = CLAMP(
+		uwNewX, g_sMouseManager.uwLoX, g_sMouseManager.uwHiX
 	);
-	g_sMouseManager.uwY = CLAMP(
-		uwNewY, g_sMouseManager.uwMinY, g_sMouseManager.uwMaxY
+	g_sMouseManager.pMouses[ubMousePort].uwY = CLAMP(
+		uwNewY, g_sMouseManager.uwLoY, g_sMouseManager.uwHiY
 	);
 }
 
 /**
  * Moves mouse pointer from current position by relative offsets.
  */
-static inline void mouseMoveBy(WORD wDx, WORD wDy) {
-	g_sMouseManager.uwX = CLAMP(
-		g_sMouseManager.uwX + wDx, g_sMouseManager.uwMinX, g_sMouseManager.uwMaxX
+static inline void mouseMoveBy(
+	IN UBYTE ubMousePort,
+	IN WORD wDx,
+	IN WORD wDy
+) {
+	g_sMouseManager.pMouses[ubMousePort].uwX = CLAMP(
+		g_sMouseManager.pMouses[ubMousePort].uwX + wDx,
+		g_sMouseManager.uwLoX, g_sMouseManager.uwHiX
 	);
-	g_sMouseManager.uwY = CLAMP(
-		g_sMouseManager.uwY + wDy, g_sMouseManager.uwMinY, g_sMouseManager.uwMaxY
+	g_sMouseManager.pMouses[ubMousePort].uwY = CLAMP(
+		g_sMouseManager.pMouses[ubMousePort].uwY + wDy,
+		g_sMouseManager.uwLoY, g_sMouseManager.uwHiY
 	);
 }
 
--- src/ace/managers/mouse.c
+++ src/ace/managers/mouse.c
@@ -6,23 +6,28 @@
 /* Globals */
 tMouseManager g_sMouseManager;
 
-static UBYTE s_ubPrevX, s_ubPrevY;
-
 /* Functions */
-void mouseCreate() {
+void mouseCreate(UBYTE ubPortFlags) {
 #ifdef AMIGA
-	g_sMouseManager.uwMinX = 0;
-	g_sMouseManager.uwMinY = 0;
-	g_sMouseManager.uwMaxX = SCREEN_PAL_WIDTH;
-	g_sMouseManager.uwMaxY = SCREEN_PAL_HEIGHT;
-	g_sMouseManager.uwX = (g_sMouseManager.uwMaxX - g_sMouseManager.uwMinX) >> 1;
-	g_sMouseManager.uwY = (g_sMouseManager.uwMaxY - g_sMouseManager.uwMinY) >> 1;
-	s_ubPrevX = 0;
-	s_ubPrevY = 0;
+	mouseSetBounds(0, 0, SCREEN_PAL_WIDTH, SCREEN_PAL_HEIGHT);
 
-	// Enable RMB & MMB on port 1
+	g_sMouseManager.ubPortFlags = ubPortFlags;
 	g_sMouseManager.uwPrevPotGo = custom.potinp;
-	UWORD uwPotMask = (1 << 11) | (1 << 10) | (1 << 9) | (1 << 8);
+	UWORD uwPotMask = 0;
+
+	// Enable RMB & MMB
+	if(ubPortFlags & MOUSE_PORT_1) {
+		uwPotMask |= (1 << 11) | (1 << 10) | (1 << 9) | (1 << 8);
+		memset(&g_sMouseManager.pMouses[MOUSE_PORT_1], 0, sizeof(tMouse));
+		g_sMouseManager.pMouses[MOUSE_PORT_1].uwX = (g_sMouseManager.uwHiX - g_sMouseManager.uwLoX) >> 1;
+		g_sMouseManager.pMouses[MOUSE_PORT_1].uwY = (g_sMouseManager.uwHiY - g_sMouseManager.uwHiY) >> 1;
+	}
+	if(ubPortFlags & MOUSE_PORT_2) {
+		uwPotMask |= (1 << 15) | (1 << 14) | (1 << 13) | (1 << 12);
+		memset(&g_sMouseManager.pMouses[MOUSE_PORT_2], 0, sizeof(tMouse));
+		g_sMouseManager.pMouses[MOUSE_PORT_2].uwX = (g_sMouseManager.uwHiX - g_sMouseManager.uwLoX) >> 1;
+		g_sMouseManager.pMouses[MOUSE_PORT_2].uwY = (g_sMouseManager.uwHiY - g_sMouseManager.uwLoY) >> 1;
+	}
 	custom.potgo = (custom.potinp & (0xFFFF ^ uwPotMask)) | uwPotMask;
 
 	// Amiga Hardware Reference Manual suggests that pos should be polled every
@@ -38,46 +43,81 @@ void mouseDestroy(void) {
 #endif // AMIGA
 }
 
-void mouseSetBounds(UWORD uwX, UWORD uwY, UWORD uwWidth, UWORD uwHeight) {
-	g_sMouseManager.uwMinX = uwX;
-	g_sMouseManager.uwMinY = uwY;
-	g_sMouseManager.uwMaxX = uwWidth;
-	g_sMouseManager.uwMaxY = uwHeight;
+void mouseSetBounds(UWORD uwLoX, UWORD uwLoY, UWORD uwHiX, UWORD uwHiY) {
+	g_sMouseManager.uwLoX = uwLoX;
+	g_sMouseManager.uwLoY = uwLoY;
+	g_sMouseManager.uwHiX = uwHiX;
+	g_sMouseManager.uwHiY = uwHiY;
 }
 
 void mouseProcess(void) {
 	// Even if whole Amiga process will be moved to vbl interrupt, other platforms
 	// will prob'ly use this fn anyway
 #ifdef AMIGA
-	UWORD uwMousePos = custom.joy0dat;
-	UBYTE ubPosX = uwMousePos & 0xFF;
-	UBYTE ubPosY = uwMousePos >> 8;
-
-	BYTE bDx = ubPosX - s_ubPrevX;
-	BYTE bDy = ubPosY - s_ubPrevY;
-	mouseMoveBy(bDx, bDy);
-
-	s_ubPrevX = ubPosX;
-	s_ubPrevY = ubPosY;
 	volatile UWORD * const pCiaAPra = (UWORD*)((UBYTE*)0xBFE001);
 
-	// Left button state
-	if(*pCiaAPra & (1 << 6))
-		mouseSetButton(MOUSE_LMB, MOUSE_NACTIVE);
-	else if(!mouseCheck(MOUSE_LMB))
-		mouseSetButton(MOUSE_LMB, MOUSE_ACTIVE);
+	if(g_sMouseManager.ubPortFlags & MOUSE_PORT_1) {
+		// Movement
+		UWORD uwMousePos = custom.joy0dat;
+		UBYTE ubPosX = uwMousePos & 0xFF;
+		UBYTE ubPosY = uwMousePos >> 8;
 
-	// Right button state
-	if(custom.potinp & (1 << 10))
-		mouseSetButton(MOUSE_RMB, MOUSE_NACTIVE);
-	else if(!mouseCheck(MOUSE_RMB))
-		mouseSetButton(MOUSE_RMB, MOUSE_ACTIVE);
+		BYTE bDx = ubPosX - g_sMouseManager.pMouses[MOUSE_PORT_1].ubPrevHwX;
+		BYTE bDy = ubPosY - g_sMouseManager.pMouses[MOUSE_PORT_1].ubPrevHwY;
+		mouseMoveBy(MOUSE_PORT_1, bDx, bDy);
 
-	// Middle button state
-	if(custom.potinp & (1 << 8))
-		mouseSetButton(MOUSE_MMB, MOUSE_NACTIVE);
-	else if(!mouseCheck(MOUSE_MMB))
-		mouseSetButton(MOUSE_MMB, MOUSE_ACTIVE);
+		g_sMouseManager.pMouses[MOUSE_PORT_1].ubPrevHwX = ubPosX;
+		g_sMouseManager.pMouses[MOUSE_PORT_1].ubPrevHwY = ubPosY;
+
+		// Left button state
+		if(*pCiaAPra & (1 << 6))
+			mouseSetButton(MOUSE_PORT_1, MOUSE_LMB, MOUSE_NACTIVE);
+		else if(!mouseCheck(MOUSE_PORT_1, MOUSE_LMB))
+			mouseSetButton(MOUSE_PORT_1, MOUSE_LMB, MOUSE_ACTIVE);
+
+		// Right button state
+		if(custom.potinp & (1 << 10))
+			mouseSetButton(MOUSE_PORT_1, MOUSE_RMB, MOUSE_NACTIVE);
+		else if(!mouseCheck(MOUSE_PORT_1, MOUSE_RMB))
+			mouseSetButton(MOUSE_PORT_1, MOUSE_RMB, MOUSE_ACTIVE);
+
+		// Middle button state
+		if(custom.potinp & (1 << 8))
+			mouseSetButton(MOUSE_PORT_1, MOUSE_MMB, MOUSE_NACTIVE);
+		else if(!mouseCheck(MOUSE_PORT_1, MOUSE_MMB))
+			mouseSetButton(MOUSE_PORT_1, MOUSE_MMB, MOUSE_ACTIVE);
+	}
+	if(g_sMouseManager.ubPortFlags & MOUSE_PORT_2) {
+		// Movement
+		UWORD uwMousePos = custom.joy1dat;
+		UBYTE ubPosX = uwMousePos & 0xFF;
+		UBYTE ubPosY = uwMousePos >> 8;
+
+		BYTE bDx = ubPosX - g_sMouseManager.pMouses[MOUSE_PORT_2].ubPrevHwX;
+		BYTE bDy = ubPosY - g_sMouseManager.pMouses[MOUSE_PORT_2].ubPrevHwY;
+		mouseMoveBy(MOUSE_PORT_2, bDx, bDy);
+
+		g_sMouseManager.pMouses[MOUSE_PORT_2].ubPrevHwX = ubPosX;
+		g_sMouseManager.pMouses[MOUSE_PORT_2].ubPrevHwY = ubPosY;
+
+		// Left button state
+		if(*pCiaAPra & (1 << 7))
+			mouseSetButton(MOUSE_PORT_2, MOUSE_LMB, MOUSE_NACTIVE);
+		else if(!mouseCheck(MOUSE_PORT_2, MOUSE_LMB))
+			mouseSetButton(MOUSE_PORT_2, MOUSE_LMB, MOUSE_ACTIVE);
+
+		// Right button state
+		if(custom.potinp & (1 << 14))
+			mouseSetButton(MOUSE_PORT_2, MOUSE_RMB, MOUSE_NACTIVE);
+		else if(!mouseCheck(MOUSE_PORT_2, MOUSE_RMB))
+			mouseSetButton(MOUSE_PORT_2, MOUSE_RMB, MOUSE_ACTIVE);
+
+		// Middle button state
+		if(custom.potinp & (1 << 12))
+			mouseSetButton(MOUSE_PORT_2, MOUSE_MMB, MOUSE_NACTIVE);
+		else if(!mouseCheck(MOUSE_PORT_2, MOUSE_MMB))
+			mouseSetButton(MOUSE_PORT_2, MOUSE_MMB, MOUSE_ACTIVE);
+	}
 
 #endif // AMIGA
 }
