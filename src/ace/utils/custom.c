4f86bf1c788bd01716627ca33d52515472c95a7a
KaiN <kain@piwnica.ws>
2018-06-24 22:10:08 +0200

    formatting

--- src/ace/utils/custom.c
+++ src/ace/utils/custom.c
@@ -11,11 +11,19 @@
 
 tCustom FAR REGPTR g_pCustom = (tCustom REGPTR)CUSTOM_BASE;
 
-tRayPos FAR REGPTR g_pRayPos = (tRayPos REGPTR)(CUSTOM_BASE + offsetof(tCustom, vposr));
+tRayPos FAR REGPTR g_pRayPos = (tRayPos REGPTR)(
+	CUSTOM_BASE + offsetof(tCustom, vposr)
+);
 
-tCopperUlong FAR REGPTR g_pBplFetch = (tCopperUlong REGPTR)(CUSTOM_BASE + offsetof(tCustom, bplpt));
-tCopperUlong FAR REGPTR g_pSprFetch = (tCopperUlong REGPTR)(CUSTOM_BASE + offsetof(tCustom, sprpt));
-tCopperUlong FAR REGPTR g_pCopLc = (tCopperUlong REGPTR)(CUSTOM_BASE + offsetof(tCustom, cop1lc));
+tCopperUlong FAR REGPTR g_pBplFetch = (tCopperUlong REGPTR)(
+	CUSTOM_BASE + offsetof(tCustom, bplpt)
+);
+tCopperUlong FAR REGPTR g_pSprFetch = (tCopperUlong REGPTR)(
+	CUSTOM_BASE + offsetof(tCustom, sprpt)
+);
+tCopperUlong FAR REGPTR g_pCopLc = (tCopperUlong REGPTR)(
+	CUSTOM_BASE + offsetof(tCustom, cop1lc)
+);
 
 tCia FAR REGPTR g_pCiaA = (tCia*)0xBFE001;
 tCia FAR REGPTR g_pCiaB = (tCia*)0xBFD000;
