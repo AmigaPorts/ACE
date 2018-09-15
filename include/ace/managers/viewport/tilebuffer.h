5ed7b843191de3b2054ecdde66158a9d33131ec6
KaiN <kain@piwnica.ws>
2018-09-15 14:17:30 +0200

    Tilebuffer is halfway uncrapped

--- include/ace/managers/viewport/tilebuffer.h
+++ include/ace/managers/viewport/tilebuffer.h
@@ -21,45 +21,74 @@
 #include <ace/managers/viewport/camera.h>
 #include <ace/managers/viewport/scrollbuffer.h>
 
+// vPort ptr
+#define TAG_TILEBUFFER_VPORT (TAG_USER|1)
+// Scrollable area bounds, in pixels
+#define TAG_TILEBUFFER_BOUND_TILE_X     (TAG_USER|2)
+#define TAG_TILEBUFFER_BOUND_TILE_Y     (TAG_USER|3)
+#define TAG_TILEBUFFER_TILE_SHIFT (TAG_USER|4)
+// Buffer bitmap creation flags
+#define TAG_TILEBUFFER_BITMAP_FLAGS (TAG_USER|5)
+#define TAG_TILEBUFFER_IS_DBLBUF    (TAG_USER|6)
+// If in raw mode, offset on copperlist for placing required copper
+// instructions, specified in copper instruction count since beginning.
+#define TAG_TILEBUFFER_COPLIST_OFFSET_START (TAG_USER|7)
+#define TAG_TILEBUFFER_COPLIST_OFFSET_BREAK (TAG_USER|8)
+// Callbacks, tileset
+#define TAG_TILEBUFFER_TILESET             (TAG_USER|9)
+#define TAG_TILEBUFFER_CALLBACK_TILE_DRAW  (TAG_USER|10)
+#define TAG_TILEBUFFER_REDRAW_QUEUE_LENGTH (TAG_USER|11)
+
 /* types */
 
-typedef void (*tCbTileDraw)(
+typedef void (*tTileDrawCallback)(
 	UWORD uwTileX, UWORD uwTileY,
 	tBitMap *pBitMap, UWORD uwBitMapX, UWORD uwBitMapY
 );
 
 typedef struct {
-	WORD wTileOffs; /// Index of row/col to update
-	WORD wTileCurr; /// Index of current tile to update in row/col
-	WORD wTileEnd;  /// Index of last+1  tile to update in row/col
-} tTileMarginData;
+	WORD wTilePos; ///< Index of row/col to update
+	WORD wTileCurr; ///< Index of current tile to update in row/col
+	WORD wTileEnd;  ///< Index of last+1  tile to update in row/col
+} tMarginState;
+
+typedef struct _tRedrawState {
+	tMarginState sMarginL; ///< Data for left margin
+	tMarginState sMarginR; ///< Ditto, right
+	tMarginState sMarginU; ///< Ditto, up
+	tMarginState sMarginD; ///< Ditto, down
+	// Vars needed in Process, reset in Create
+	tMarginState *pMarginX;         ///< Idx of X margin to be redrawn
+	tMarginState *pMarginOppositeX; ///< Opposite margin of pMarginX
+	tMarginState *pMarginY;         ///< Idx of Y margin to be redrawn
+	tMarginState *pMarginOppositeY; ///< Opposite margin of pMarginY
+	// Tile redraw queue
+	tUwCoordYX *pPendingQueue;
+	UBYTE ubPendingCount;
+} tRedrawState;
 
 typedef struct {
 	tVpManager sCommon;
-	tCameraManager *pCameraManager;       /// Quick ref to Camera
-	tScrollBufferManager *pScrollManager; /// Quick ref to Scroll
-	                                      // Manager vars
-	tUwCoordYX uTileBounds;               /// Tile count in x,y
-	UBYTE ubTileSize;                     /// Tile size in pixels
-	UBYTE ubTileShift;                    /// Tile size in shift, e.g. 4 for 16: 1 << 4 == 16
-	UWORD uwMarginedWidth;                /// Width of visible area + margins
-	UWORD uwMarginedHeight;               /// Height of visible area + margins
-	                                      /// TODO: refresh when scrollbuffer changes
-	tCbTileDraw cbTileDraw;              /// Called when tile is redrawn
-	UBYTE **pTileData;                    /// 2D array of tile indices
-	tBitMap *pTileSet;                    /// Tileset
-	                                      // Margin vars
-	UBYTE ubMarginXLength;                /// Tile number in margins: left & right
-	UBYTE ubMarginYLength;                /// Ditto, up & down
-	tTileMarginData sMarginL;             /// Data for left margin
-	tTileMarginData sMarginR;             /// Ditto, right
-	tTileMarginData sMarginU;             /// Ditto, up
-	tTileMarginData sMarginD;             /// Ditto, down
-	                                      // Vars needed in Process, reset in Create
-	tTileMarginData *pMarginX;            /// Idx of X margin to be redrawn
-	tTileMarginData *pMarginOppositeX;    /// Opposite margin of pMarginX
-	tTileMarginData *pMarginY;            /// Idx of Y margin to be redrawn
-	tTileMarginData *pMarginOppositeY;    /// Opposite margin of pMarginY
+	tCameraManager *pCamera;       ///< Quick ref to Camera
+	tScrollBufferManager *pScroll; ///< Quick ref to Scroll
+	// Manager vars
+	tUwCoordYX uTileBounds;       ///< Tile count in x,y
+	UBYTE ubTileSize;             ///< Tile size in pixels
+	UBYTE ubTileShift;            ///< Tile size in shift, e.g. 4 for 16: 1 << 4 == 16
+	UWORD uwMarginedWidth;        ///< Width of visible area + margins
+	UWORD uwMarginedHeight;       ///< Height of visible area + margins
+	                              ///  TODO: refresh when scrollbuffer changes
+	tTileDrawCallback cbTileDraw; ///< Called when tile is redrawn
+	UBYTE **pTileData;            ///< 2D array of tile indices
+	tBitMap *pTileSet;            ///< Tileset - one tile beneath another
+	// Margin & queue geometry
+	UBYTE ubMarginXLength; ///< Tile number in margins: left & right
+	UBYTE ubMarginYLength; ///< Ditto, up & down
+	UBYTE ubQueueSize;
+	// Redraw state and double buffering
+	tRedrawState pRedrawStates[2];
+	UBYTE ubStateIdx;
+
 } tTileBufferManager;
 
 /* globals */
@@ -70,10 +99,7 @@ typedef struct {
  * Tilemap buffer manager create fn
  * Be sure to set camera pos, load tile data & then call tileBufferRedraw()!
  */
-tTileBufferManager *tileBufferCreate(
-	tVPort *pVPort, UWORD uwTileX, UWORD uwTileY, UBYTE ubTileShift,
-	tBitMap *pTileset, tCbTileDraw cbTileDraw
-);
+tTileBufferManager *tileBufferCreate(void *pTags, ...);
 
 void tileBufferDestroy(tTileBufferManager *pManager);
 
@@ -84,14 +110,15 @@ void tileBufferDestroy(tTileBufferManager *pManager);
 void tileBufferProcess(tTileBufferManager *pManager);
 
 void tileBufferReset(
-	tTileBufferManager *pManager, UWORD uwTileX, UWORD uwTileY, tBitMap *pTileset
+	tTileBufferManager *pManager, UWORD uwTileX, UWORD uwTileY,
+	UBYTE ubBitmapFlags, UBYTE isDblBuf
 );
 
 /**
  * Redraws tiles on whole screen
  * Use for init or something like that, as it's slooooooooow
  */
-void tileBufferRedraw(tTileBufferManager *pManager);
+void tileBufferInitialDraw(const tTileBufferManager *pManager);
 
 /**
  * Redraws selected tile, calls custom redraw callback
@@ -99,7 +126,7 @@ void tileBufferRedraw(tTileBufferManager *pManager);
  * Use for single redraws
  */
 void tileBufferDrawTile(
-	tTileBufferManager *pManager, UWORD uwTileIdxX, UWORD uwTileIdxY
+	const tTileBufferManager *pManager, UWORD uwTileIdxX, UWORD uwTileIdxY
 );
 
 /**
@@ -108,7 +135,7 @@ void tileBufferDrawTile(
  * Use for batch redraws with smart uwBfrXY update
  */
 void tileBufferDrawTileQuick(
-	tTileBufferManager *pManager,
+	const tTileBufferManager *pManager,
 	UWORD uwTileIdxX, UWORD uwTileIdxY, UWORD uwBfrX, UWORD uwBfrY
 );
 
@@ -119,6 +146,14 @@ void tileBufferDrawTileQuick(
 void tileBufferInvalidateRect(
 	tTileBufferManager *pManager, UWORD uwX, UWORD uwY,
 	UWORD uwWidth, UWORD uwHeight
+);
+
+void tileBufferInvalidateTile(
+	tTileBufferManager *pManager, UWORD uwTileX, UWORD uwTileY
+);
+
+UBYTE tileBufferIsTileOnBuffer(
+	const tTileBufferManager *pManager, UWORD uwTileX, UWORD uwTileY
 );
 
 #endif // AMIGA
