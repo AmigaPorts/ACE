72c7c36cd7c3eb38687c81b6200fd633f658ba34
KaiN <kain@piwnica.ws>
2019-01-03 07:56:47 +0100

    added endian conversion util

---
+++ tools/font_conv/endian.h
@@ -0,0 +1,41 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <cstdint>
+
+namespace nEndian {
+	constexpr bool isBig(void)
+	{
+		// Based on https://stackoverflow.com/questions/1001307/
+		union {
+				uint32_t i;
+				char c[4];
+		} uTest = {0x01020304};
+		return uTest.c[0] == 1;
+	}
+
+	constexpr uint16_t toBig16(uint16_t uwIn)
+	{
+		if(isBig()) {
+			return uwIn;
+		}
+		return (
+			((uwIn & 0xFF'00) >> 8) |
+			((uwIn & 0x00'FF) << 8)
+		);
+	}
+
+	constexpr uint32_t toBig32(uint32_t ulIn)
+	{
+		if(isBig()) {
+			return ulIn;
+		}
+		return (
+			((ulIn & 0xFF'00'00'00) >> 24) |
+			((ulIn & 0x00'FF'00'00) >>  8) |
+			((ulIn & 0x00'00'FF'00) <<  8) |
+			((ulIn & 0x00'00'00'FF) << 24)
+		);
+	}
+}
