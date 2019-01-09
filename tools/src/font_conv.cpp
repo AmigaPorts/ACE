503c6c01a0837cf9478ffa122ced6af58b48fdd6
KaiN <kain@piwnica.ws>
2019-01-09 16:34:24 +0100

    added usage

--- tools/src/font_conv.cpp
+++ tools/src/font_conv.cpp
@@ -4,12 +4,35 @@
 
 #include <map>
 #include <fmt/format.h>
-#include "tools/font_converter.h"
+#include "tools/glyph_set.h"
 
-int main(void)
+static const std::string s_szDefaultCharset = (
+	" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+);
+
+void printUsage(const std::string &szAppName) {
+	using fmt::print;
+	print("Usage:\n\t{} inPath outType [extraOpts]\n", szAppName);
+	print("\ninPath\t- path to TTF file or directory with PNG glyphs\n");
+	print("outType\t- one of following:\n");
+	print("\tdir\tDirectory with each glyph as separate PNG\n");
+	print("\tpng\tSingle PNG file with whole font\n");
+	print("\tfnt\tACE font file\n");
+	print("extraOpts:\n");
+	// -chars
+	print("\t-chars \"AB D\"\tInclude only 'A', 'B', ' ' and 'D' chars in font.\n");
+	print("\t\t\tUse backslash (\\) to escape quote (\") char inside charset specifier\n");
+	print("\t\t\tDefault charset is: \"{}\"\n", s_szDefaultCharset);
+	// -out
+	// -op
+	print("\t-out outPath\tSpecify output path, including file name.\n");
+	print("\t\t\tDefault is same name as input with changed extension\n");
+}
+
+int main(int lArgCount, const char *pArgs[])
 {
-	std::string szCharset =
-		" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	const uint8_t ubMandatoryArgCnt = 1;
+	// Mandatory args
 
 	auto mGlyphs = tGlyphSet::fromTtf("./arial.ttf", 20, szCharset, 128);
 	// auto mGlyphs = tGlyphSet::fromDir("./letters");
