0cec2479ac9b5f90545e914eca39a18134794042
KaiN <kain@piwnica.ws>
2019-05-22 16:31:16 +0200

    global variables

---
+++ src/ace/utils/ptplayer.c
@@ -0,0 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <ace/utils/ptplayer.h>
+
+UBYTE mt_MusicChannels = 0;
+UBYTE mt_E8Trigger = 0;
+UBYTE mt_Enable = 0;
