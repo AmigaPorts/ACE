d3402de6b5dd10caed4acaeca5e40b2d73a33d7a
KaiN <kain@piwnica.ws>
2020-06-03 19:21:10 +0200

    added strchr, memcmp and _putchar for printf

---
+++ src/mini_std/string.c
@@ -0,0 +1,29 @@
+#include <string.h>
+
+int memcmp(const void *pLhs, const void *pRhs, size_t count) {
+	// https://en.cppreference.com/w/c/string/byte/memcmp
+	unsigned char *pLeft = (unsigned char *)pLhs;
+	unsigned char *pRight = (unsigned char *)pRhs;
+	while(count--) {
+		int delta = *pLeft - *pRight;
+		if(delta != 0) {
+			return delta;
+		}
+		++pLhs;
+		++pRhs;
+	}
+	return 0;
+}
+
+unsigned long strtoul(const char *restrict str, char **restrict str_end, int base);
+
+char *strchr(const char *szHaystack, int cNeedle) {
+	// https://en.cppreference.com/w/c/string/byte/strchr
+	while(*szHaystack != '\0') {
+		if(*szHaystack == cNeedle) {
+			return (char*)szHaystack;
+		}
+		++szHaystack;
+	}
+	return 0;
+}
