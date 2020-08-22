7d6f047e2016c385ac10a4b816d320c3db30d7c6
KaiN <kain@piwnica.ws>
2020-08-22 12:20:41 +0200

    added stringDecimalFromULong()

---
+++ include/ace/utils/string.h
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef _ACE_UTILS_STRING_H_
+#define _ACE_UTILS_STRING_H_
+
+// Contains highly optimized string functions.
+// Those should be preferred over standard library in game loop.
+
+#include <ace/types.h>
+
+char *stringDecimalFromULong(ULONG ulVal, char *pDst);
+
+#endif // _ACE_UTILS_STRING_H_
