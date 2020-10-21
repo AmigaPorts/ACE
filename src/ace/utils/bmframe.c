329f280068d9d41d890ec28dc23876234406e493
KaiN <kain@piwnica.ws>
2020-10-21 10:46:16 +0200

    bmframe revamp

--- src/ace/utils/bmframe.c
+++ src/ace/utils/bmframe.c
@@ -4,53 +4,73 @@
 
 #include <ace/utils/bmframe.h>
 
+typedef enum _tBorderTile {
+	BORDER_TILE_NW = 0,
+	BORDER_TILE_N = 1,
+	BORDER_TILE_NE = 2,
+	BORDER_TILE_W = 3,
+	BORDER_TILE_MID = 4,
+	BORDER_TILE_E = 5,
+	BORDER_TILE_SW = 6,
+	BORDER_TILE_S = 7,
+	BORDER_TILE_SE = 8,
+} _tBorderTile;
+
 void bmFrameDraw(
-	tBitMap *pFrameSet, tBitMap *pDest,
+	const tBitMap *pFrameSet, tBitMap *pDest,
 	UWORD uwX, UWORD uwY, UBYTE ubCols, UBYTE ubRows, UBYTE ubTileSize
 ) {
 	// Vertices
-	blitCopyAligned(pFrameSet, 0, 0, pDest, uwX, uwY, ubTileSize, ubTileSize);
 	blitCopyAligned(
-		pFrameSet, ubTileSize * 2, 0, pDest,
+		pFrameSet, 0, BORDER_TILE_NW * ubTileSize, pDest,
+		uwX, uwY, ubTileSize, ubTileSize
+	);
+	blitCopyAligned(
+		pFrameSet, 0, BORDER_TILE_NE * ubTileSize, pDest,
 		uwX + ((ubCols - 1) * ubTileSize), uwY, ubTileSize, ubTileSize
 	);
 	blitCopyAligned(
-		pFrameSet, 0, ubTileSize * 2, pDest,
+		pFrameSet, 0, BORDER_TILE_SW * ubTileSize, pDest,
 		uwX, uwY + ((ubRows - 1) * ubTileSize), ubTileSize, ubTileSize
 	);
 	blitCopyAligned(
-		pFrameSet, ubTileSize * 2, ubTileSize * 2, pDest,
+		pFrameSet, 0, BORDER_TILE_SE * ubTileSize, pDest,
 		uwX + ((ubCols - 1) * ubTileSize), uwY + ((ubRows - 1) * ubTileSize),
 		ubTileSize, ubTileSize
 	);
+
 	// Horizontal edges
 	for(UBYTE i = 1; i < ubCols-1; ++i) {
 		blitCopyAligned(
-			pFrameSet, ubTileSize, 0, pDest, uwX+(i * ubTileSize), uwY,
+			pFrameSet, 0, BORDER_TILE_N * ubTileSize, pDest,
+			uwX + (i * ubTileSize), uwY,
 			ubTileSize, ubTileSize
 		);
 		blitCopyAligned(
-			pFrameSet, ubTileSize, ubTileSize * 2, pDest, uwX + (i * ubTileSize),
-			uwY+((ubRows-1) * ubTileSize), ubTileSize, ubTileSize
+			pFrameSet, 0, BORDER_TILE_S * ubTileSize, pDest,
+			uwX + (i * ubTileSize), uwY + ((ubRows - 1) * ubTileSize),
+			ubTileSize, ubTileSize
 		);
 	}
-	// Center
+
+	// Middle rows
 	if(ubRows > 2) {
 		// Draw only first row
 		blitCopyAligned(
-			pFrameSet, 0, ubTileSize, pDest,
+			pFrameSet, 0, BORDER_TILE_W * ubTileSize, pDest,
 			uwX, uwY + ubTileSize, ubTileSize, ubTileSize
 		);
 		blitCopyAligned(
-			pFrameSet, ubTileSize * 2, ubTileSize, pDest,
+			pFrameSet, 0, BORDER_TILE_E * ubTileSize, pDest,
 			uwX + ((ubCols-1) * ubTileSize), uwY + ubTileSize, ubTileSize, ubTileSize
 		);
 		for(UBYTE i = 1; i < ubCols-1; ++i) {
 			blitCopyAligned(
-				pFrameSet, ubTileSize, ubTileSize, pDest,
+				pFrameSet, 0, BORDER_TILE_MID * ubTileSize, pDest,
 				uwX + (i * ubTileSize), uwY + ubTileSize, ubTileSize, ubTileSize
 			);
 		}
+
 		// Fill rest of rows with the first one
 		for(UBYTE i = 2; i < ubRows - 1; ++i) {
 			blitCopyAligned(
