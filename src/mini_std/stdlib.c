16a15620ecd26006a6664c1a74357777ed82e32a
KaiN <kain@piwnica.ws>
2020-10-29 11:22:49 +0100

    added qsort() replacement to mini_std

--- src/mini_std/stdlib.c
+++ src/mini_std/stdlib.c
@@ -1,4 +1,7 @@
 #include <proto/dos.h>
+#include <sort_r.h>
+
+static int (*s_qsortComp)(const void *, const void *);
 
 void exit(int exit_code) {
 	// FIXME: implement better, Exit() shouldn't be used.
@@ -7,4 +10,17 @@ void exit(int exit_code) {
 
 	Exit(exit_code);
 	while(1) {}
+}
+
+int qsortEmu(const void *_a, const void *_b, __attribute__((unused)) void *_arg) {
+	return s_qsortComp(_a, _b);
+}
+
+void qsort(
+	void *ptr, size_t count, size_t size, int (*comp)(const void *, const void *)
+) {
+	// sort_r requires extra arg so call fixed callback which calls regular qsort cb
+
+	s_qsortComp = comp;
+	sort_r(ptr, count, size, qsortEmu, 0);
 }
