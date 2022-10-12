97d96817941420e52b6f9b7dcec4d6ae3c37c5aa
Rem <remz@ludusdesign.com>
2022-10-11 23:47:23 -0400

    In ptPlayerDestroy, disable the appropriate timer A interrupt (either VBL, or CIA_B-TimerA)

--- src/ace/managers/ptplayer.c
+++ src/ace/managers/ptplayer.c
@@ -1646,8 +1646,8 @@ void ptplayerDestroy(void) {
 	ptplayerStop();
 	// Disable handling of music
 	ptplayerEnableMusic(0);
-	systemSetCiaInt(CIA_B, 0, 0, 0);
-	systemSetCiaInt(CIA_B, 1, 0, 0);
+	ptplayerEnableMainHandler(0);
+	systemSetCiaInt(CIA_B, CIAICRB_TIMER_B, 0, 0);
 	// systemSetInt(INTB_AUD0, 0, 0);
 	// systemSetInt(INTB_AUD1, 0, 0);
 	// systemSetInt(INTB_AUD2, 0, 0);
