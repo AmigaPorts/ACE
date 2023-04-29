ae59f2fcb2686f45065cd90dadbb93a58846e968
KaiN <kain@piwnica.ws>
2023-04-29 10:53:24 +0200

    review fixes

--- src/ace/utils/palette.c
+++ src/ace/utils/palette.c
@@ -76,7 +76,7 @@ void paletteDump(UWORD *pPalette, UBYTE ubColorCnt, char *szPath) {
 		++ubBpp;
 	}
 	tBitMap *pBm = bitmapCreate(
-		SNAP_UP((1+8)*ubColorCnt + 1, 16), 10, ubBpp, BMF_CLEAR
+		CEIL_TO_FACTOR((1+8)*ubColorCnt + 1, 16), 10, ubBpp, BMF_CLEAR
 	);
 	for(UBYTE i = 0; i <= ubColorCnt; ++i) {
 		blitRect(pBm, 1+(8+1)*i, 1, 8, 8, i);
