c4d725d3ce7826b48751277b8c4b46575328dc96
Karol Wnuk <k.wnuk@ascetic.pl>
2026-06-02 23:04:13 +0200

    Add MPL license preamble

--- showcase/src/test/diagnostics.c
+++ showcase/src/test/diagnostics.c
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include "diagnostics.h"
 #include "test/simple_buffer_bpp.h"
 #include "test/scroll_tile_buffer.h"
--- showcase/src/test/scroll_tile_buffer.h
+++ showcase/src/test/scroll_tile_buffer.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef _DIAGNOSTICS_SCROLL_TILE_BUFFER_H_
 #define _DIAGNOSTICS_SCROLL_TILE_BUFFER_H_
 
